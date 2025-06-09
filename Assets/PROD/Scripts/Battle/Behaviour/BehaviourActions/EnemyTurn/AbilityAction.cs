76a9e1d0ee54a50879fd171ef5c40d5e3a50a9c8
Floko <flo.kot@free.fr>
2025-06-09 05:03:14 +0200

    Counter strikes

--- Assets/PROD/Scripts/Battle/Behaviour/BehaviourActions/EnemyTurn/AbilityAction.cs
+++ Assets/PROD/Scripts/Battle/Behaviour/BehaviourActions/EnemyTurn/AbilityAction.cs
@@ -1,11 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using BitDuc.Demo;
-using BitDuc.EnhancedTimeline.Timeline;
 using R3;
 using Unity.Behavior;
-using Unity.Cinemachine;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
@@ -14,11 +11,13 @@ using Unity.Properties;
 [NodeDescription(name: "Ability", story: "[Unit] uses [ability] on [Targets]", category: "Action",
     id: "285316aaf1c489624db0457ded13ddb2")]
 public partial class AbilityAction : Action {
+    
     [SerializeReference] public BlackboardVariable<Unit> Unit;
     [SerializeReference] public BlackboardVariable<AbilityData> Ability;
     [SerializeReference] public BlackboardVariable<List<GameObject>> Targets;
 
     private bool _hasCinematicEnded;
+    private bool _isCounterAvailable;
     private IDisposable _abilityExecution;
     
     protected override Status OnStart() {
@@ -27,13 +26,17 @@ public partial class AbilityAction : Action {
 
         _hasCinematicEnded = false;
         var battleManager = Toolbox.Get<BattleManager>();
+        
+        _isCounterAvailable = true;
         _abilityExecution = battleManager.ExecuteAbility(Unit.Value, Targets.Value.Select(u => u.GetComponent<Unit>()).ToList(), Ability.Value).Subscribe(
             onNext: _ => { },
-            onCompleted: _ => _hasCinematicEnded = true
+            onCompleted: _ => {
+                if (_isCounterAvailable && Ability.Value.targetMode is AbilityTargetMode.SelectTarget) Counter();
+                else _hasCinematicEnded = true;
+            }
         );
         
         return Status.Running;
-        
     }
 
     protected override Status OnUpdate() {
@@ -41,7 +44,19 @@ public partial class AbilityAction : Action {
     }
 
     protected override void OnEnd() {
-        _abilityExecution.Dispose();
+        _abilityExecution?.Dispose();
+    }
+    
+    private void Counter() {
+        var battleManager = Toolbox.Get<BattleManager>();
+        var singleTarget = Targets.Value[0].GetComponent<Unit>() as AllyUnit;
+        singleTarget.DodgeSystem.enabled = false;
+            
+        BattleLogDebugUI.Log(singleTarget.unitData.counterAbility.desc);
+        _abilityExecution = battleManager.ExecuteAbility(singleTarget, new List<Unit> {Unit.Value}, singleTarget.unitData.counterAbility).Subscribe(
+            onNext: _ => { },
+            onCompleted: _ => { _hasCinematicEnded = true; }
+        );
     }
 }
 
